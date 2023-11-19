dc494b18e9920f368a35b5c1e35bc2be7b73717e
Murad Yarmammadov <yarmamedov.murad01@gmail.com>
2023-11-19 17:10:48 +0100

    UI and Physics completed

--- Angry Birds Clone/GameScene.swift
+++ Angry Birds Clone/GameScene.swift
@@ -10,79 +10,167 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var bird = SKSpriteNode()
+    var background = SKSpriteNode()
+    var tree = SKSpriteNode()
+    var brick1 = SKSpriteNode()
+    var brick2 = SKSpriteNode()
+    var brick3 = SKSpriteNode()
+    var brick4 = SKSpriteNode()
+    var brick5 = SKSpriteNode()
+    var brick6 = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        setUI()
+    }
+    
+    func setUI(){
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.scene?.scaleMode = .aspectFit
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        //Background
+        let backgroundTexture = SKTexture(imageNamed: "background")
+        background = SKSpriteNode(texture: backgroundTexture)
+        background.position = CGPoint(x: 0, y: 0)
+        background.size = CGSize(width: self.frame.width, height: self.frame.height)
+        background.zPosition = -1
+        self.addChild(background)
+        
+        // Bird
+        let birdTexture = SKTexture(imageNamed: "bird")
+        bird = SKSpriteNode(texture: birdTexture)
+        bird.position = CGPoint(x: -(self.frame.width / 3.2), y: -(self.frame.height / 7))
+        bird.size = CGSize(width: self.frame.width / 11, height: self.frame.height / 7)
+        bird.zPosition = 1
+        bird.zRotation = 0.4
+        self.addChild(bird)
+        
+        //Bird Physics
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: (birdTexture.size().height / 4) - 5)
+        bird.physicsBody?.affectedByGravity = false
+        bird.physicsBody?.isDynamic = true
+        bird.physicsBody?.mass = 0.3
+        
+        //Tree
+        let treeTexture = SKTexture(imageNamed: "tree")
+        tree = SKSpriteNode(texture: treeTexture)
+        tree.position = CGPoint(x: -(self.frame.width / 3.5), y: -(self.frame.height/5))
+        tree.size = CGSize(width: self.frame.width/3, height: self.frame.height/1.3)
+        tree.zPosition = 0
+        self.addChild(tree)
+        
+        //Brickes
+        let brickTextures = SKTexture(imageNamed: "brick")
+        
+        brick1 = SKSpriteNode(texture: brickTextures)
+        brick1.position = CGPoint(x: self.frame.width/5, y: -(self.frame.height/2.2))
+        brick1.size = CGSize(width: self.frame.width / 14, height: self.frame.height / 8)
+        brick1.zPosition = 1
+        self.addChild(brick1)
+        
+        brick2 = SKSpriteNode(texture: brickTextures)
+        brick2.position = CGPoint(x: ((self.frame.width / 5) + (self.frame.width / 14)), y: -(self.frame.height/2.2))
+        brick2.size = CGSize(width: self.frame.width / 14, height: self.frame.height / 8)
+        brick2.zPosition = 1
+        self.addChild(brick2)
+
+        brick3 = SKSpriteNode(texture: brickTextures)
+        brick3.position = CGPoint(x: ((self.frame.width / 5) + (self.frame.width / 7)), y: -(self.frame.height/2.2))
+        brick3.size = CGSize(width: self.frame.width / 14, height: self.frame.height / 8)
+        brick3.zPosition = 1
+        self.addChild(brick3)
+
+        brick4 = SKSpriteNode(texture: brickTextures)
+        brick4.position = CGPoint(x: (self.frame.width / 5) + (self.frame.width / 28) , y: -(self.frame.height/2.2) + (self.frame.height / 8))
+        brick4.size = CGSize(width: self.frame.width / 14, height: self.frame.height / 8)
+        brick4.zPosition = 1
+        self.addChild(brick4)
+        
+        brick5 = SKSpriteNode(texture: brickTextures)
+        brick5.position = CGPoint(x: (self.frame.width / 5) + 3*(self.frame.width / 28) , y: -(self.frame.height/2.2) + (self.frame.height / 8))
+        brick5.size = CGSize(width: self.frame.width / 14, height: self.frame.height / 8)
+        brick5.zPosition = 1
+        self.addChild(brick5)
+        
+        brick6 = SKSpriteNode(texture: brickTextures)
+        brick6.position = CGPoint(x: (self.frame.width / 5) + (self.frame.width / 14) , y: -(self.frame.height/2.2) + (self.frame.height / 4))
+        brick6.size = CGSize(width: self.frame.width / 14, height: self.frame.height / 8)
+        brick6.zPosition = 1
+        self.addChild(brick6)
+        
+        //Bricks Physics
+        let brickSize = CGSize(width: brickTextures.size().width/2, height: (birdTexture.size().height/2) - 10)
+        
+        brick1.physicsBody = SKPhysicsBody(rectangleOf: brickSize)
+        brick1.physicsBody?.isDynamic = true
+        brick1.physicsBody?.affectedByGravity = false
+        brick1.physicsBody?.allowsRotation = true
+        brick1.physicsBody?.mass = 0.4
+        
+        brick2.physicsBody = SKPhysicsBody(rectangleOf: brickSize)
+        brick2.physicsBody?.isDynamic = true
+        brick2.physicsBody?.affectedByGravity = true
+        brick2.physicsBody?.allowsRotation = true
+        brick2.physicsBody?.mass = 0.4
+
+        brick3.physicsBody = SKPhysicsBody(rectangleOf: brickSize)
+        brick3.physicsBody?.isDynamic = true
+        brick3.physicsBody?.affectedByGravity = true
+        brick3.physicsBody?.allowsRotation = true
+        brick3.physicsBody?.mass = 0.4
+
+        brick4.physicsBody = SKPhysicsBody(rectangleOf: brickSize)
+        brick4.physicsBody?.isDynamic = true
+        brick4.physicsBody?.affectedByGravity = true
+        brick4.physicsBody?.allowsRotation = true
+        brick4.physicsBody?.mass = 0.4
+
+        brick5.physicsBody = SKPhysicsBody(rectangleOf: brickSize)
+        brick5.physicsBody?.isDynamic = true
+        brick5.physicsBody?.affectedByGravity = true
+        brick5.physicsBody?.allowsRotation = true
+        brick5.physicsBody?.mass = 0.4
+        
+        brick6.physicsBody = SKPhysicsBody(rectangleOf: brickSize)
+        brick6.physicsBody?.isDynamic = true
+        brick6.physicsBody?.affectedByGravity = true
+        brick6.physicsBody?.allowsRotation = true
+        brick6.physicsBody?.mass = 0.4
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
     }
 }
